bfae667429d7cccfdca0c7b550bffb0093c621b2
Oliver Korpilla <oliver.korpilla@gmx.de>
2018-11-08 16:12:57 +0000

    aligning with exercises in book

--- test/Main.purs
+++ test/Main.purs
@@ -4,9 +4,19 @@ import Prelude
 import Effect (Effect)
 import Test.Assert (assert)
 import Data.Monoid.Additive (Additive(..))
+import Data.Monoid.Multiplicative (Multiplicative(..))
 import Monoid.Optional (Optional(..))
+
+sum :: forall a. a -> Additive a
+sum = Additive
+
+product :: forall a. a -> Multiplicative a
+product = Multiplicative
 
 main :: Effect Unit
 main = do
-  assert (Only (Additive 7) <> Only (Additive 3) == Only (Additive 10))
-  assert (Nada <> Only (Additive 5) <> Nada      == Only (Additive 5))
+  assert (Only (sum 1)     `append` Only (sum 1)     == Only (sum 2))
+  assert (Only (product 4) `append` Only (product 2) == Only (product 8))
+  assert (Only (sum 1)     `append` Nada             == Only (sum 1))
+  assert (Only [1]         `append` Nada             == Only [1])
+  assert (Nada             `append` Only (sum 1)     == Only (sum 1))
